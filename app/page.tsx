f15531649aa6a18d5ce794131b528d586ee74f9c
Jefferson Fonseca <drpcscan@gmail.com>
2024-09-04 16:31:59 -0300

    feat: start maps

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,30 @@
-
-import { Heading } from "@/components/core/Typograph/Heading";
+'use client'
+import MapboxExample from "@/components/Map";
+import { Button } from "@nextui-org/button";
+import { useState } from "react";
 
 export default function Home() {
+  const [coord, setCoord] = useState<number[]>([-43.566333,-22.903654])
+  const [list, setList] = useState(true)
   return (
-    <section className="flex justify-between">
-    <div className="flex flex-col">
-    <Heading size='small' title="Primeira Pagina" />
-      <Heading size='medium' title="Primeira Pagina 1" />
-      <Heading size='large' title="Primeira Pagina 2" />
+    <section className=" flex justify-between">
+  <div className="flex relative w-full h-full">
+  <div className=" flex absolute top-2 right-2 z-10 ">
+      <Button onClick={()=> setList((e)=> !e)}>{list ? "Listagem" : 'Map'}</Button>
 
-    </div>
-      <div style={{
-        display: 'flex',
-        background: 'black',
-        padding: '64px'
-      }} >
-      <div className="w-64 h-64 bg-red-500">
-
-</div>
       </div>
+      <MapboxExample coord={coord} />
+      </div>
+    {!list && <div className=" flex flex-col mx-2 gap-4">
+      <Button onClick={()=> setCoord([-43.566333,-22.903654])}>Default</Button>
+      <Button onClick={()=> setCoord([-43.044827,-22.681616])}>NewCoord</Button>
+      <Button onClick={()=> setCoord([-44.044827,-22.681616])}>NewCoord</Button>
+      <Button onClick={()=> setCoord([-45.044827,-22.681616])}>NewCoord</Button>
+      <Button onClick={()=> setCoord([-46.044827,-22.681616])}>NewCoord</Button>
+      <Button onClick={()=> setCoord([-47.044827,-22.681616])}>NewCoord</Button>
+      <Button onClick={()=> setCoord([-48.044827,-22.681616])}>NewCoord</Button>
+      <Button onClick={()=> setCoord([-49.044827,-22.681616])}>NewCoord</Button>
+    </div>}
 
     </section>
   );
