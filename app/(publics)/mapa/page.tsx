f99e2bf8c7fcbc8f738a0810812587540393d7dc
Jefferson Fonseca <drpcscan@gmail.com>
2024-11-21 19:10:56 -0300

    fix: ads

--- app/(publics)/mapa/page.tsx
+++ app/(publics)/mapa/page.tsx
@@ -1,8 +1,9 @@
+'use server'
 import MapAndList from "./_features/MapAndList";
 
 export default async function Page(){
 
-  const data = await fetch('/api/list-occurrence')
+  const data = await fetch('https://front-protegeo.vercel.app/api/list-occurrence')
 
   const rows = (await data.json()).rows
 
