7c933e408cd8b33368286de6bcf2850777f07c78
Jefferson Fonseca <drpcscan@gmail.com>
2025-05-01 16:22:30 -0300

    fix: ajuste

--- app/api/list-occurrence/route.ts
+++ app/api/list-occurrence/route.ts
@@ -6,10 +6,7 @@ import { NextRequest, NextResponse } from 'next/server';
 export async function POST(request: NextRequest) {
   const query = `SELECT * FROM ocorrencias;`;
 
-  
-  
   const rows = await sql.query(query)
-  
 
   return NextResponse.json({rows});
 }
